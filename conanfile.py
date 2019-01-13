1592433d559126dd54f92522be9c56ff2a80e7de
Stephen Wray <wrayste+github.com@gmail.com>
2019-01-13 10:49:09 +0000

    Add header only option

--- conanfile.py
+++ conanfile.py
@@ -9,6 +9,7 @@ class BlazeConan(ConanFile):
     generators = "cmake"
     url = 'http://github.com/wrayste/conan-blaze'
     gz_file_sha = "d66abaf4633d60b6e6472f6ecd7db7b4fb5f74a4afcfdf00c92e1ea61f2e0870"
+    options = {"header_only": true}
 
     def source(self):
         gz_name = "blaze-%s.tar.gz" % (self.version)
